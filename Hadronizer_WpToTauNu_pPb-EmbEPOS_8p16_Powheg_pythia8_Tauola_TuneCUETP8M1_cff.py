76d9ac8e77bd1ff09eb25601e7715a37ba7112e6
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2017-08-24 11:19:32 +0200

    Add WToTau Powheg Hadronizer Fragments with Tauola

---
+++ Hadronizer_WpToTauNu_pPb-EmbEPOS_8p16_Powheg_pythia8_Tauola_TuneCUETP8M1_cff.py
@@ -0,0 +1,47 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.Pythia8PowhegEmissionVetoSettings_cfi import *
+from GeneratorInterface.ExternalDecays.TauolaSettings_cff import *
+
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc530/8TeVpPb/Powheg/V2/Wp_taunu_8p16TeV_pPb_CT14_EPPS16_W_ew-BMNNP.tgz'),
+#    args = cms.vstring("PATH/TO/TARBALL/GRIDPACK.tgz"),
+    nEvents = cms.untracked.uint32(1),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         filterEfficiency = cms.untracked.double(1.0),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         comEnergy = cms.double(8160.),
+                         ExternalDecays = cms.PSet(
+                            Tauola = cms.untracked.PSet(
+                               TauolaPolar,
+                               TauolaDefaultInputCards
+                               ),
+                            parameterSets = cms.vstring('Tauola')
+                            ),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        pythia8PowhegEmissionVetoSettingsBlock,
+        processParameters = cms.vstring(
+            'POWHEG:nFinal = 1',   ## Number of final state particles
+                                   ## (BEFORE THE DECAYS) in the LHE
+                                   ## other than emitted extra parton
+          ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'pythia8PowhegEmissionVetoSettings',
+                                    'processParameters'
+                                    )
+        )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
